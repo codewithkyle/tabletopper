780db8c0cdac551b82ee1ad3a4dc48cae67b6bfc
Kyle Andrews <codingwithkyle@gmail.com>
2022-02-09 15:57:16 -0500

    ♻ Refactored service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,43 +1,68 @@
-self.importScripts('./service-worker-assets.js');
+self.addEventListener("install", (event) => event.waitUntil(onInstall(event)));
+self.addEventListener("activate", (event) =>
+    event.waitUntil(onActivate(event))
+);
+self.addEventListener("fetch", (event) => event.respondWith(onFetch(event)));
 
-self.addEventListener('install', event => event.waitUntil(onInstall(event)));
-self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
-
-const cacheNamePrefix = 'resource-cache-';
-const cacheName = `${cacheNamePrefix}${self.manifest.version}`;
+let cacheNamePrefix = "resource-cache";
+let cacheName = "";
 
 // Cache files when the service worker is installed or updated
 async function onInstall(event) {
     self.skipWaiting();
-    const assetsRequests = self.manifest.assets.map(asset => {
-        return new Request(asset, {
-            cache: "reload",
-        });
-    });
-    for (const request of assetsRequests){
-        await caches.open(cacheName).then(cache => cache.add(request)).catch(error => {
-            console.error("Failed to cache:", request, error);
-        });
-    }
+    console.log("SW Installed");
 }
 
 // Cleanup old caches
 async function onActivate(event) {
-    const cacheKeys = await caches.keys();
-    await Promise.all(cacheKeys
-        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-        .map(key => caches.delete(key)));
+    console.log("SW Activated");
 }
 
 // Try to respond with cached files
 async function onFetch(event) {
-    if (event.request.method === 'GET' && event.request.url.indexOf(self.origin) === 0) {
+    if (event.request.url.indexOf(self.origin) === 0) {
         const cache = await caches.open(cacheName);
         const cachedResponse = await cache.match(event.request);
-        if (cachedResponse){
+        if (cachedResponse) {
             return cachedResponse;
         }
     }
     return fetch(event.request);
 }
+
+self.addEventListener("message", async (event) => {
+    cacheName = `${cacheNamePrefix}-${event.data.version}`;
+    const cacheKeys = await caches.keys();
+    await Promise.all(
+        cacheKeys
+            .filter(
+                (key) => key.startsWith(cacheNamePrefix) && key !== cacheName
+            )
+            .map((key) => caches.delete(key))
+    );
+    if (event.data?.assets) {
+        const assetsRequests = event.data.assets.map((asset) => {
+            return new Request(asset, {
+                cache: "reload",
+            });
+        });
+        for (const request of assetsRequests) {
+            await caches
+                .open(cacheName)
+                .then((cache) => cache.add(request))
+                .catch((error) => {
+                    console.error("Failed to cache:", request, error);
+                });
+        }
+    }
+    self.clients
+        .matchAll({
+            includeUncontrolled: true,
+            type: "window",
+        })
+        .then((clients) => {
+            if (clients && clients.length) {
+                clients[0].postMessage(event.data.version);
+            }
+        });
+});
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -6,7 +6,31 @@ import { connect } from "~controllers/ws";
     // @ts-ignore
     const { ENV } = await import("/config.js");
     if (ENV === "production"){
-        await navigator.serviceWorker.register('service-worker.js');
+        await new Promise<void>(resolve => {
+            navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
+            navigator.serviceWorker.addEventListener("message", (e) => {
+                localStorage.setItem("version", e.data);
+            });
+            navigator.serviceWorker.ready.then(async (registration) => {
+                try {
+                    await import("/service-worker-assets.js?t=" + Date.now());
+                    // @ts-expect-error
+                    if (self.manifest.version !== localStorage.getItem("version")) {
+                        localStorage.removeItem("version");
+                    } else {
+                        // @ts-expect-error
+                        delete self.manifest.assets;
+                    }
+                    // @ts-expect-error
+                    registration.active.postMessage(self.manifest);
+                } catch (e) {
+                    registration.active.postMessage({
+                        version: localStorage.getItem("version") || "init",
+                    });
+                }
+                resolve();
+            });
+        });
     }
 
     // @ts-expect-error
