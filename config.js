478609f30b6b6743151b1657e8ca45301acb544e
Kyle Andrews <codingwithkyle@gmail.com>
2022-02-12 12:58:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const ENV = "production";
-export const SOCKET_URL = "wss://ws.tabletopper.app/";
+export const SOCKET_URL = "ws://ws.tabletopper.app/";
 export const SERVER_URL = "https://ws.tabletopper.app/";
