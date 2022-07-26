89b00cf17d6390c78e28a63c3070f12c70ac3cbd
Kyle Andrews <codingwithkyle@gmail.com>
2022-07-26 12:58:25 -0400

    ♻ Refactored router logic to use new v2.1.0 Router lib

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,13 @@
-import { configure, mount } from "@codewithkyle/router";
+import { router, mount } from "@codewithkyle/router";
 
 (() => {
+    // Begin router
+    router.add("/", "home-page");
+    router.add("/room/{CODE}", "tabletop-page");
+    router.add("*", "missing-page");
+
     document.body.innerHTML = "";
     const main = document.createElement("main");
     document.body.appendChild(main);
     mount(main);
-
-    configure({
-        "/": "home-page",
-        "/room/{CODE}": "tabletop-page",
-        "404": "missing-page",
-    });
 })();
