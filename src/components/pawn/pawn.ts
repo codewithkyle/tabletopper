e03950df64b38c564a717140c5a29e0f7c8ac60c
Kyle Andrews <codingwithkyle@gmail.com>
2022-11-16 18:56:25 -0500

    ✨ Added dead pawn

--- src/components/pawn/pawn.ts
+++ src/components/pawn/pawn.ts
@@ -131,7 +131,10 @@ export default class Pawn extends SuperComponent<IPawn>{
 
     private resetTooltip(){
         if (this.model.hp !== null && this.model.fullHP !== null){
-            if (this.model.hp <= this.model.fullHP * 0.5){
+            if (this.model.hp === 0){
+                this.setAttribute("bleeding", "false");
+                this.setAttribute("tooltip", `${this.model.name} (dead)`);
+            } else if (this.model.hp <= this.model.fullHP * 0.5){
                 this.setAttribute("bleeding", "true");
                 this.setAttribute("tooltip", `${this.model.name} (bloody)`);
             } else {
@@ -191,6 +194,20 @@ export default class Pawn extends SuperComponent<IPawn>{
 
     private renderPawn():TemplateResult{
         let out:TemplateResult;
+        if ("hp" in this.model && this.model.hp === 0){
+            return html`
+                <div pawn="dead">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                       <path d="M12 4c4.418 0 8 3.358 8 7.5c0 1.901 -.755 3.637 -1.999 4.96l-.001 2.54a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1v-2.54c-1.245 -1.322 -2 -3.058 -2 -4.96c0 -4.142 3.582 -7.5 8 -7.5z"></path>
+                       <path d="M10 17v3"></path>
+                       <path d="M14 17v3"></path>
+                       <circle cx="9" cy="11" r="1"></circle>
+                       <circle cx="15" cy="11" r="1"></circle>
+                    </svg>
+                </div>
+            `;
+        }
         if (this.model.image){
             out = html`
                 <img src="${this.model.image}" alt="${this.model.name} token" draggable="false">
