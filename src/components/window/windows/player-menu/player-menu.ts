5bef5ecd4790f6a1d56257534a6e94c56d5c2577
Kyle Andrews <codingwithkyle@gmail.com>
2023-06-19 14:58:36 -0400

    🐛 Fixed player rename bug

--- src/components/window/windows/player-menu/player-menu.ts
+++ src/components/window/windows/player-menu/player-menu.ts
@@ -4,9 +4,11 @@ import SuperComponent from "@codewithkyle/supercomponent";
 import { html, render, TemplateResult } from "lit-html";
 import Input from "~brixi/components/inputs/input/input";
 import env from "~brixi/controllers/env";
-import Modal from "~components/modal/modal";
+//import Modal from "~components/modal/modal";
 import cc from "~controllers/control-center";
 import { send } from "~controllers/ws";
+import modals from "~brixi/controllers/modals";
+import Form from "~brixi/components/form/form";
 
 interface IPlayerMenu {};
 export default class PlayerMenu extends SuperComponent<IPlayerMenu>{
@@ -39,35 +41,34 @@ export default class PlayerMenu extends SuperComponent<IPlayerMenu>{
         send("room:player:ban", target.dataset.uid);
     }
 
-    private handleRenameSubmit:EventListener = (e:SubmitEvent) => {
-        const form = e.currentTarget as HTMLFormElement;
-        const input:Input = form.querySelector("[form-input]");
-        const name = input.getValue();
-        const op = cc.set("players", form.dataset.playerUid, "name", name);
+    private handleRenameSubmit(data:any, form:Form, modal:HTMLElement, playerUid:string) {
+        const name = data["name"];
+        send("room:player:rename", {
+            playerId: playerUid,
+            name: name,
+        });
+        const op = cc.set("players", playerUid, "name", name);
         cc.dispatch(op);
-        const modal = form.closest("modal-component");
+        form.stop();
         modal.remove();
     }
 
     private renamePlayer:EventListener = (e:Event) => {
         const target = e.currentTarget as HTMLElement;
-        const modal = new Modal("medium");
-        modal.setHeading("Rename Player");
-        modal.setView(html`
-            <form @submit=${this.handleRenameSubmit.bind(this)} data-player-uid="${target.dataset.uid}">
-                <div class="w-full px-1 pb-1 pt-0.5">
-                    ${new Input({
-                        name: "name",
-                        value: target.dataset.name,
-                    })}
-                </div>
-                <div class="actions">
-                    <button class="bttn mr-1" kind="solid" color="white" type="button" @click=${modal.close.bind(modal)}>cancel</button>
-                    <button class="bttn" kind="solid" color="success" type="submit">update</button>
-                </div>
-            </form>
-        `);
-        document.body.append(modal);
+        modals.form({
+            title: "Rename Player",
+            view: html`
+                ${new Input({
+                    name: "name",
+                    value: target.dataset.name,
+                })}
+            `,
+            callbacks: {
+                submit: (data, form, modal) => {
+                    this.handleRenameSubmit(data, form, modal, target.dataset.uid);
+                },
+            },
+        });
     }
 
     private mutePlayer:EventListener = (e:Event) => {
