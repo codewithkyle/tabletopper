28f4e4152f3766bc25e57ba7408d247c97411313
Kyle Andrews <codingwithkyle@gmail.com>
2026-03-01 10:21:17 -0500

    Added calculation helpers

---
+++ server/internal/helpers/calculations.go
@@ -0,0 +1,36 @@
+package helpers
+
+import (
+	"math"
+	"strconv"
+)
+
+func CalculateModifier(base int) string {
+	modifier := math.Floor(float64(base-10) / 2)
+	modifierStr := strconv.Itoa(int(modifier))
+	if modifier >= 0 {
+		return "+" + modifierStr
+	} else {
+		return modifierStr
+	}
+}
+
+func CalculateProficiencyBonus(cr int) string {
+	if cr >= 0 && cr <= 4 {
+		return "+2"
+	} else if cr >= 5 && cr <= 8 {
+		return "+3"
+	} else if cr >= 9 && cr <= 12 {
+		return "+4"
+	} else if cr >= 13 && cr <= 16 {
+		return "+5"
+	} else if cr >= 17 && cr <= 20 {
+		return "+6"
+	} else if cr >= 21 && cr <= 24 {
+		return "+7"
+	} else if cr >= 26 && cr <= 28 {
+		return "+8"
+	} else {
+		return "+9"
+	}
+}
