8f9053ab0a86f91a8f5d7be571c7bab97ebbb9c6
Kyle Andrews <codingwithkyle@gmail.com>
2026-03-02 15:14:24 -0500

    Closes #52 - added global HTMX loading indicators

--- client/src/notif.ts
+++ client/src/notif.ts
@@ -1,5 +1,21 @@
 import notifications from "~brixi/controllers/alerts";
+import env from "~brixi/controllers/env";
+
+env.boot();
 
 window.addEventListener("toast", (event:CustomEvent) => {
     notifications.toast(event.detail?.msg ?? event.detail.value);
+});
+
+const tickets = [];
+document.body.addEventListener("htmx:beforeRequest", (e) => {
+    console.log("starting htmx request");
+    const ticket = env.startLoading();
+    tickets.push(ticket);
+});
+document.body.addEventListener("htmx:afterRequest", (e) => {
+    console.log("done with request");
+    for (let i = tickets.length-1; i >= 0; i--) {
+        env.stopLoading(tickets[i]);
+    }
 });
